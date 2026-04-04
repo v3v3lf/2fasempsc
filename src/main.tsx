654fce81a81c588c3cf8eba6c0eca54617c2fa76
v3v3lf <110268372+v3v3lf@users.noreply.github.com>
2026-04-04 20:09:55 -0300

    Simplify App: clean version to test rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { ErrorBoundary } from './ErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 );
